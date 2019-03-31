787ddd3077b856ca746abba9a449511948da0cf6
JShimonov <jshimonov@gmail.com>
2019-03-31 19:55:39 -0400

    Done with Part 2

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Joseph Shimonov on 3/31/19.
+//  Copyright © 2019 Joseph Shimonov. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Podfile
+++ Podfile
@@ -8,6 +8,7 @@ target 'Parstagram' do
   # Pods for Parstagram
   pod "Parse"
   pod "AlamofireImage"
+  pod "MessageInputBar"
 
   target 'ParstagramTests' do
     inherit! :search_paths
